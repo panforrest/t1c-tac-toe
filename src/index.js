2223dea0b35aac5e2e0ae2280eaa43bbb79fd9e0
panforrest <guoqianp@gmail.com>
2017-07-26 23:53:23 -0400

    Board use two loops instead of hardcoding

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class Board extends React.Component {
   renderSquare(i){
 	  return (
   	  <Square 
+        key = {i}
   	    value={this.props.squares[i]} 
         onClick={() => this.props.onClick(i)}
   	  />
@@ -21,26 +22,18 @@ class Board extends React.Component {
   }
 
   render() {
+    var wrapper = [];
+    for (var i=0; i<=2; i++){
+      var row = [];
+      for (var j=3 * i; j<=3 * i + 2; j++){
+        row.push(this.renderSquare(j));
+      }
+      wrapper.push(<div className="board-row" key={i}>{row}</div>);
+    }
     return (
   	  <div>
-  	    <div className="board-row">
-  	      {this.renderSquare(0)}
-  	      {this.renderSquare(1)}
-  	      {this.renderSquare(2)}
-  	    </div>
-
-  	    <div className="board-row">
-  	      {this.renderSquare(3)}
-  	      {this.renderSquare(4)}
-  	      {this.renderSquare(5)}
-  	    </div>
-
-  	    <div className="board-row">
-  	      {this.renderSquare(6)}
-  	      {this.renderSquare(7)}
-  	      {this.renderSquare(8)}
-  	    </div>
-
+        <div className="status">{/*status*/}</div>
+        {wrapper}
   	  </div>
 	);
   }
@@ -95,7 +88,7 @@ class Game extends React.Component {
       const clickedLocation = step.clickedLocation
       const desc = move ? 'Move #' + move + ' (' + clickedLocation[0] + ',' + clickedLocation[1] + ')' : 'Game start';
 
-      if (move == history.length - 1) {
+      if (move === history.length - 1) {
         return (
           <li key={move}>
             <a href="#" onClick={() => this.jumpTo(move)} style={{fontWeight:"bold", fontSize:"22px"}} >{desc}</a> 
